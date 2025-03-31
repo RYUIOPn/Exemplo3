1fdda7d5e0c3dc7d8956187c9cfc8ee5e8a23689
Rafael <seijiarakaki12@gmail.com>
2025-03-31 09:13:43 -0300

    tipos de variaveis

---
+++ scr/js/index.script.js
@@ -0,0 +1,26 @@
+//declarações e variaveis 
+
+
+var idade = "19"
+console.log(idade)
+
+const sobrenome = "fiapinho"
+console.log(sobrenome)
+let nome1 
+console.log(nome1)
+
+let nome2 =null
+console.log(nome2)
+
+let exemplo1 = 10
+console.log(typeof exemplo1)
+let exemplo2 = "tecnologia"
+console.log(typeof exemplo2)
+let exemplo3 = true
+console.log(typeof exemplo3)
+let exemplo4 = {}
+console.log(typeof exemplo4)
+let exemplo5 = []
+console.log(typeof exemplo5)
+
+let palavra = prompt("escreva algo")
