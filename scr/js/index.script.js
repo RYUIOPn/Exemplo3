ec6d97d71ec1ef576bc0586217b2557eb4ab4841
Rafael <seijiarakaki12@gmail.com>
2025-03-31 09:54:07 -0300

    operadores aritmeticos de comparação

--- scr/js/index.script.js
+++ scr/js/index.script.js
@@ -1,26 +1,67 @@
 //declarações e variaveis 
 
 
-var idade = "19"
-console.log(idade)
+// var idade = "19"
+// console.log(idade)
 
-const sobrenome = "fiapinho"
-console.log(sobrenome)
-let nome1 
-console.log(nome1)
+// const sobrenome = "fiapinho"
+// console.log(sobrenome)
+// let nome1 
+// console.log(nome1)
 
-let nome2 =null
-console.log(nome2)
+// let nome2 =null
+// console.log(nome2)
 
-let exemplo1 = 10
-console.log(typeof exemplo1)
-let exemplo2 = "tecnologia"
-console.log(typeof exemplo2)
-let exemplo3 = true
-console.log(typeof exemplo3)
-let exemplo4 = {}
-console.log(typeof exemplo4)
-let exemplo5 = []
-console.log(typeof exemplo5)
+// let exemplo1 = 10
+// console.log(typeof exemplo1)
+// let exemplo2 = "tecnologia"
+// console.log(typeof exemplo2)
+// let exemplo3 = true
+// console.log(typeof exemplo3)
+// let exemplo4 = {}
+// console.log(typeof exemplo4)
+// let exemplo5 = []
+// console.log(typeof exemplo5)
 
-let palavra = prompt("escreva algo")
+// //converções 
+// let numfloat = 123.456
+// console.log(parseInt(numfloat))
+
+// let numString ="123.456"
+// console.log(parseFloat(numString))
+
+// //metodos
+
+// //lenght =verifica o tamanho da string
+// let frase = "o mundo da tecnologia"
+// console.log(frase.length)
+
+// //indexof -retorna um trecho do texto
+// let texto ="a programação indomavel"
+// console.log(texto.indexOf("ão"))
+
+
+// //slice -retorna um trecho do texto com inicio e final 
+// let info ="processamento de dados"
+// console.log(info.slice(8,16))
+
+// //conhecendo alguns metodos 
+
+// //operadores aritmeticos 
+
+const num1 = 10
+const num2 = 20
+
+// console.log(num1 + num2)
+// console.log(num1 - num2)
+
+//operadores logicos 
+
+console.log(num1 < num2)
+console.log(num1 > num2 && num2 < num1)
+console.log(num1 >num2 || num2 < num1)
+
+//operadores de comparação 
+
+console.log (num1 == num2)
+console.log (num1 === num2)
